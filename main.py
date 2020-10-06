3a0cbbc66bf2477cad1470d3b1ddee00bb0b398e
yaonam <elimviolinist@gmail.com>
2020-10-06 12:26:56 -0400

    Refined print statements

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with sr.Microphone() as source:
 
 # recognize speech using Sphinx
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    print("Sphinx thinks you said: '" + r.recognize_sphinx(audio) + "'")
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
@@ -22,7 +22,7 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    print("Google Speech Recognition thinks you said: '" + r.recognize_google(audio) + "'")
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
